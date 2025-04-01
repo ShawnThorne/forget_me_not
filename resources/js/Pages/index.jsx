27f6ffa404c5f54204e2ea8f4e43a86e07f2561d
Shawn T <shawn.thorne9@gmail.com>
2025-03-31 19:16:08 -0600

    initial commit

---
+++ resources/js/Pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Index() {
+    return (
+        <div>
+            <h1 className="font-bold">Welcome to the Main Page</h1>
+            <p>This is the start of forget me not</p>
+        </div>
+    );
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @inertiaHead
+</head>
+<body>
+    @inertia
+</body>
+</html>
